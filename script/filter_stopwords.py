16ceaac1b6dbc686f9c2a692beba83d2bd05f5f7
Gengyi Sun <gengyi.sun@mail.mcgill.ca>
2019-10-08 02:32:51 -0400

    Occurrences

--- script/filter_stopwords.py
+++ script/filter_stopwords.py
@@ -18,6 +18,7 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer 
+from nltk.stem.snowball import SnowballStemmer
 # 2.0   Count word Frequency in each subreddit
 #       Generating all frequency files
 #------------------------------------------------------------------------------
@@ -26,6 +27,10 @@ from nltk.stem import WordNetLemmatizer
 file_name_df = pd.read_csv('../data/subreddits.csv')
 lemmatizer = WordNetLemmatizer() 
 
+
+def lemmatize_text(text):
+    return [lemmatizer.lemmatize(w) for w in text]
+
 for name in file_name_df['0']:
     file_df = pd.read_csv('../data/parsed_data/{}.csv'.format(name))
     print("---------------------------File Loaded-----------------------------")
@@ -33,17 +38,18 @@ for name in file_name_df['0']:
     # 2.1 Clean comments
     #------------------------------------------------------------------------------
     stop_words = set(stopwords.words('english')) 
-    delete_symbol_token = file_df['comments'].str.replace('[{}]'.format(string.punctuation), '')
-    #delete_stopword_token 
-    lemmanized_token= delete_symbol_token.str.lower().apply(lambda x: [item for item in str(x).split() if item not in stop_words])
-    #lemmanized_token = delete_stopword_token.apply(lambda x: [lemmatizer.lemmatize(w) for w in str(x).split()])
+    stemmer = SnowballStemmer("english")
+
+    file_df['delete_symbol_token'] = file_df['comments'].str.replace('[{}]'.format(string.punctuation), '')
+    file_df['delete_stopword_token']= file_df['delete_symbol_token'].str.lower().apply(lambda x: [item for item in str(x).split() if item not in stop_words])
+    file_df['text_lemmatized'] = file_df.delete_stopword_token.apply(lambda x : [lemmatizer.lemmatize(w) for w in x])
     print("--------------------------Data Processed---------------------------")
 
    # 2.2 Get unique words in comments
     #------------------------------------------------------------------------------
     i = 0
     list_of_word = []
-    for word in lemmanized_token:
+    for word in file_df['text_lemmatized']:
         # Run through the list checking for existeing word
         for element in list_of_word :
             if word == element:
@@ -79,7 +85,7 @@ for name in file_name_df['0']:
     # 2.4 Concade all words in one list
     #------------------------------------------------------------------------------
     new_word_list = []
-    for word in lemmanized_token:
+    for word in file_df['text_lemmatized']:
         new_word_list = new_word_list + word
     
     word_df = pd.DataFrame(data = new_word_list)
--- script/lemma_tester.py
+++ script/lemma_tester.py
@@ -18,21 +18,24 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer 
+from nltk.stem.snowball import SnowballStemmer
 
+stemmer = SnowballStemmer("english")
 
 w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
 lemmatizer = WordNetLemmatizer() 
 
 
 def lemmatize_text(text):
-    return lemmatizer.lemmatize(text)
+    return [lemmatizer.lemmatize(w) for w in text]
 
 
 df = pd.DataFrame(['this was cheesy', 'she likes these books', 'wow this is great'], columns=['text'])
 df['text_lemmatized'] = df.text.apply(lemmatize_text)
 
-file_df = pd.read_csv('../data/subreddits.csv')
+file_df = pd.read_csv('../data/parsed_data/hockey.csv')
 stop_words = set(stopwords.words('english')) 
-delete_symbol_token = file_df['0'].str.replace('[{}]'.format(string.punctuation), '')
-delete_stopword_token= delete_symbol_token.str.lower().apply(lambda x: [item for item in str(x).split() if item not in stop_words])
-lemmanized_token = delete_stopword_token.apply(lambda x : [lemmatize_text(w) for w in str(x).split])
+file_df['delete_symbol_token'] = file_df['comments'].str.replace('[{}]'.format(string.punctuation), '')
+file_df['delete_stopword_token']= file_df['delete_symbol_token'].str.lower().apply(lambda x: [item for item in str(x).split() if item not in stop_words])
+file_df['text_lemmatized'] = file_df.delete_stopword_token.apply(lemmatize_text)
+#lemmanized_token = delete_stopword_token.apply(lambda x : [lemmatize_text(w) for w in str(x).split])
