27620d67648abdcac881076c432d163d75501aed
Han Zhou <44248469+albat3ross@users.noreply.github.com>
2019-10-11 21:19:12 -0400

    123

--- script/NaiveBayes.py
+++ script/NaiveBayes.py
@@ -2,64 +2,73 @@ import numpy as np
 import math
 
 class NaiveBayes:
-    def __init__(self, ls = True):
+    def __init__(self, num_class = 2, ls = True):
         self.ls = ls
-
+        self.num_class = num_class
         
         
     def fit(self, X_features, Y_quality):
         self.X_features = X_features
         self.Y_outcomes = Y_quality
         self.n,self.m = self.X_features.shape
-        N0 = 0
-        N1 = 0
+        self.N = np.zeros((self.num_class),dtype=int)
         for i in range(self.Y_outcomes.shape[0]):
-            if self.Y_outcomes[i] == 0:
-                N0 += 1
-            else:
-                N1 += 1
-        self.N = np.array([N0,N1])
+            self.N[self.Y_outcomes[i]]+=1
         
-        self.w = np.zeros([2,self.m],dtype=int)
+        
+        self.w = np.zeros([self.num_class,self.m],dtype=int)
         self.theta = np.ones_like(self.w,dtype = float)
         self.ww = np.ones_like(self.w,dtype = float)
+        self.logp = np.ones_like(self.w,dtype = float)
         # make array w that stores all features p(xi|y=c)
         for j in range (self.m):
             for i in range(self.n):
                 if self.X_features[i,j] == 1:
-                    if self.Y_outcomes[i]== 0:
-                        self.w[0,j]=self.w[0,j]+1
-                    else:
-                        self.w[1,j]=self.w[1,j]+1
+                    clas = self.Y_outcomes[i]
+                    self.w[clas,j] += 1
                         
         for j in range(self.m):
-            for i in range(2):
+            for i in range(self.num_class):
                 self.theta[i,j]=self.w[i,j]/self.N[i]
         if self.ls == True :
             self.laplace_smooth()
-        for j in range(self.m):
+        if self.num_class == 2:
+            for j in range(self.m):
                 self.ww[0,j] = math.log((1-self.theta[1,j])/(1-self.theta[0,j]))
                 self.ww[1,j] = math.log((self.theta[1,j])/(self.theta[0,j]))
+        else:
+            pass
+                
                 
  
                
     def laplace_smooth(self):
         for j in range(self.m):
-            for i in range(2):
+            for i in range(self.num_class):
                 self.theta[i,j]=(self.w[i,j]+1)/(self.N[i]+2)
 
         
     def predict(self, dataset):
-        XtW = np.zeros_like(dataset)
-        for i in range(dataset.shape[0]):
-            for j in range(self.m):
-                XtW[i,j] =(self.ww[1,j]-self.ww[0,j])*dataset[i,j]
-        decision = np.zeros([dataset.shape[0]], dtype = int)
-        for i in range(dataset.shape[0]):
-            result = math.log(self.N[1]/self.N[0])+ self.ww[0].sum() + XtW[i].sum()
-            if result > 0:
-                decision[i]=1
-            else:
-                decision[i]=0
-        return decision
+        if self.num_class == 2:
+            
+            XtW = np.zeros_like(dataset)
+            for i in range(dataset.shape[0]):
+                for j in range(self.m):
+                    XtW[i,j] =(self.ww[1,j]-self.ww[0,j])*dataset[i,j]
+            decision = np.zeros([dataset.shape[0]], dtype = int)
+            for i in range(dataset.shape[0]):
+                result = math.log(self.N[1]/self.N[0])+ self.ww[0].sum() + XtW[i].sum()
+                if result > 0:
+                    decision[i]=1
+                else:
+                    decision[i]=0
+            return decision
         
+        else:
+            class_prob = np.zeros((self.num_class),dtype=int)
+            for i in range(self.num_class):
+                likeli = 0
+                for j in range(self.m):
+                    likeli += dataset[j]*np.log(self.theta[i,j])+(1-dataset[j])*np.log(1-theta[i,j])
+                class_prob[i]=likeli+np.log(self.N[i]/self.n)
+            return np.argmax(class_prob)
--- script/TFIDF_workspace.py
+++ script/TFIDF_workspace.py
@@ -15,6 +15,10 @@ from nltk.stem import WordNetLemmatizer
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+import NaiveBayes as nb
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.datasets import make_classification
+from sklearn.naive_bayes import MultinomialNB
 
 #------------------------------------------------------------------------------
 # 1.1 Load file
@@ -28,9 +32,10 @@ def lemmatize_text(text):
 
 #for name in file_name_df['0']:
     
-
+'''
 file_dfidf = pd.read_csv('../data/original_data/reddit_train.csv')
 print(file_dfidf.shape)
+'''
 # 2.1 Clean comments
 #------------------------------------------------------------------------------
 stop_words = set(stopwords.words('english')) 
@@ -41,25 +46,57 @@ file_dfidf['delete_stopword_token']= file_dfidf['delete_symbol_token'].str.lower
 file_dfidf['text_lemmatized'] = file_dfidf.delete_stopword_token.apply(lambda x : [lemmatizer.lemmatize(w) for w in x])
 file_dfidf['text_stemmized'] = file_dfidf['text_lemmatized'].apply(lambda x : [stemmer.stem(w) for w in x])
 '''
-
+'''
 corpus = file_dfidf['comments']
 vectorizer1 = TfidfVectorizer(stop_words=stop_words)
 X1 = vectorizer1.fit_transform(corpus)
 wnl = WordNetLemmatizer()
 snss = SnowballStemmer("english", ignore_stopwords=True)
 my_stop_words = [snss.stem(wnl.lemmatize(t)) for t in stopwords.words('english')]
+
+'''
+    
+    
+
+
+'''
 class LemmaTokenizer(object):
     def __init__(self):
         self.wnl = WordNetLemmatizer()
         self.sns = SnowballStemmer("english", ignore_stopwords=True)
     def __call__(self, articles):
+        articles.split();
         
 
 
 vectorizer2 = TfidfVectorizer(tokenizer = LemmaTokenizer(),stop_words=my_stop_words)
 X2 = vectorizer2.fit_transform(corpus)
-    
+'''
+def binarize(X):
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            if X[i,j]>0:
+                X[i,j]=1
+            else:
+                X[i,j]=0
 
+def test():
+    diff = 0
+    nb_samples = 3000
+    for i in range(100):
+        bnbdata_X, bnbdata_Y = make_classification(n_samples=nb_samples, n_features=8, n_informative=4, n_redundant=0)
+        binarize(bnbdata_X)
+        bnb = MultinomialNB()
+        y_pred2 = bnb.fit(bnbdata_X,bnbdata_Y).predict(bnbdata_X)
+        mnb = nb.NaiveBayes()
+        mnb.fit(bnbdata_X,bnbdata_Y)
+        y_pred3 = mnb.predict(bnbdata_X)
+        print("mnb: ",(bnbdata_Y != y_pred3).sum(),"bnb: ",(bnbdata_Y != y_pred2).sum())
+        if ((bnbdata_Y != y_pred3).sum()!=(bnbdata_Y != y_pred2).sum()):
+            diff+=1
+    return diff
+
+print("test error rate out of 100 round: %d" %(test()))
 
 
 
